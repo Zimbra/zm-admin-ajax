c847b699672010323bad3e3723c8d48cd3531438
Koichi Kato <koichi.kato@synacor.com>
2025-12-16 09:53:13 +0900

    zimbrax-2.81 ZCS-18382: removed csrfToken from soap header

--- WebRoot/js/zimbra/csfe/ZmCsfeCommand.js
+++ WebRoot/js/zimbra/csfe/ZmCsfeCommand.js
@@ -427,10 +427,6 @@ function(params) {
 		throw new ZmCsfeException(ZMsg.authRequired, ZmCsfeException.NO_AUTH_TOKEN, params.methodNameStr);
 	}
 
-	if (window.csrfToken) {
-		context.csrfToken = window.csrfToken;
-	}
-
 	AjxDebug.logSoapMessage(params);
 	DBG.dumpObj(AjxDebug.DBG1, obj);
 
@@ -544,10 +540,6 @@ function(params) {
 	}
 	else if (ZmCsfeCommand.noAuth && !params.ignoreAuthToken) {
 		throw new ZmCsfeException(ZMsg.authRequired, ZmCsfeException.NO_AUTH_TOKEN, params.methodNameStr);
-	}
-
-	if (window.csrfToken) {
-		soapDoc.set("csrfToken", window.csrfToken, context);
 	}
 
 	AjxDebug.logSoapMessage(params);
